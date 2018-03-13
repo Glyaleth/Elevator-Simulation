c54b825b742f770465fdea1267b21e96518dc5fa
saally61 <37127822+saally61@users.noreply.github.com>
2018-03-13 12:45:35 -0500

    Update Passenger.h

--- Passenger.h
+++ Passenger.h
@@ -1,22 +1,24 @@
 #pragma once
 
 class Passenger {
-private: 
+private:
 	int ArrivalTime, CurrentFloor, Destination, ExitTime, WaitTime, Direction, IDNum;
 
-public: 
-	Passenger(int arrive = 0, int current=-1, int destin=-1, int ID=0);
+public:
+	Passenger(int arrive =-1, int ID = -1, int numFloor=-1);
 	int getArrivalTime(); //The time the passenger arrived
 	int getCurrentFloor(); //The floor the passenger is on
 	int getDestination(); //The floor the passenger wants to go
 	int getExitTime(int ET); //sets and returns time the passenger gets off the elevator
 	int getWaitTime(); //calculates, sets, and returns wait time based on currently assigned Arrive/Exit Time
-	int getDirection(); //sets and returns Direction based on current/destination floor. 1=up, 0=down, -1=error-->current == destination floor 
+	int getDirection(int numFloor); //sets and returns Direction based on current/destination floor. 1=up, 0=down, -1=error-->current == destination floor 
 	int getIDNum();
 
 	void setArrivalTime(int AT);
+	void setExitTime(int ET);
 	void setCurrentFloor(int CF);
 	void setDestination(int Destin);
+	void setDirection(int D);
 	void setIDNum(int ID);
 
 
