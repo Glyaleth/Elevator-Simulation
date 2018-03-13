a610bc38ab2b6a42b1ec1ce8319203502a07b7f9
saally61 <37127822+saally61@users.noreply.github.com>
2018-03-13 12:07:05 -0500

    Update Building.cpp

--- Building.cpp
+++ Building.cpp
@@ -1,12 +1,12 @@
 #include "Building.h"
 
-Building::Building()
+Building::Building(int e_num, int p_num)
 {
-	elevators = 0;
-	floors = 0;
+	elevators = e_num;
+	floors = p_num;
 }
 
-int Building::get_elevators() 
+int Building::get_elevators() const
 {
 	return elevators;
 }
@@ -18,7 +18,7 @@ int Building::get_floors()
 
 void Building::set_elevators(int e)
 {
-	elevators = e; 
+	elevators = e;
 }
 
 void Building::set_floors(int f)
