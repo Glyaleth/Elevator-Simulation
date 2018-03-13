9cbfe8dcfa5140abb60515886b477a0c2938c7e1
vivianz99 <37127916+vivianz99@users.noreply.github.com>
2018-03-13 12:18:23 -0500

    Building

--- Building.cpp
+++ Building.cpp
@@ -1,27 +1,65 @@
-#include "Building.h"
+//
+//  Elevator_manager.cpp
+//  elevator
+//
 
-Building::Building(int e_num, int p_num)
-{
-	elevators = e_num;
-	floors = p_num;
+//
+
+#include "Building.h"
+#include "Elevator.h"
+#include "Passenger.h"
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+/*void get_entry_pass(){
+    cout<<"initially,how many passenger entered the elevator"<<endl;
+    cin<<entry_passengers;
+}*/
+void passenger_entry(int i){
+    entry_passengers+=i;
+    
+}
+void passenger_exit(int n){
+    exit_passengers+=n;
+}
+int current_pass_in_ele(){
+    return entry_passengers-exit_passengers;
 }
 
-int Building::get_elevators() const
+Building::Building()
 {
-	return elevators;
+    elevators = 0;
+    floors = 0;
+}
+
+int Building::get_elevators()
+{
+    return elevators;
 }
 
 int Building::get_floors()
 {
-	return floors;
+    return floors;
 }
 
 void Building::set_elevators(int e)
 {
-	elevators = e;
+    elevators = e;
 }
 
 void Building::set_floors(int f)
 {
-	floors = f;
+    floors = f;
 }
+void Elevator_Floor(e_num,f_num){
+    vector<Elevator>Elevator(e_num);
+    vector<queue<Passenger>>Floor;//queue stores the passenger waiting at that floor
+    Floor.push_back(queue<Passenger>()); // add a queue
+    Floor[0].push(Passenger());
+}
+
+
+
+
--- Building.h
+++ Building.h
@@ -1,14 +1,35 @@
+//
+//  main.cpp
+//  building manager
+
 #pragma once
+#include "main.h"
 #include <iostream>
 using namespace std;
 
-class Building {
+
+
+class Building
+{
+protected:
+    int entry_passengers;
+    int exit_passengers;
+    int elevators, floors;
 public:
-	Building(int e_num, int p_num); //default constructor
-	int get_elevators() const;
-	int get_floors();
-	void set_elevators(int e);
-	void set_floors(int f);
-private:
-	int elevators, floors;
+    //void get_entry_pass();
+    int current_pass_in_ele();
+    void passenger_entry(int i);
+    void passenger_exit(int n);
+    Building(); //default constructor
+    int get_passengers();
+    int get_elevators();
+    int get_floors();
+    int set_passengers(int p);
+    void set_elevators(int e);
+    void set_floors(int f);
+    void Elevator_Floor(e_num,f_num)
+    
 };
+
+
+
