1988b2ea6649287f5177ec87f30b71fbab4c15fe
saally61 <37127822+saally61@users.noreply.github.com>
2018-03-13 12:46:20 -0500

    Update Passenger.cpp

--- Passenger.cpp
+++ Passenger.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <string>
+#include <ctime>
 #include "Passenger.h"
 using namespace std;
 
-Passenger::Passenger(int arrive, int current, int destin, int ID)
+Passenger::Passenger(int arrive, int ID, int numFloor)
 {
+	srand(time(NULL));
 	ArrivalTime = arrive;
-	CurrentFloor = current;
-	Destination = destin;
-	getDirection();
+	CurrentFloor = rand() % numFloor + 1; //random number from 1-number of floors 
+	Destination = rand() % numFloor + 1; //random number from 1-number of floors 
 	IDNum = ID;
 }
 
@@ -39,14 +40,15 @@ int Passenger::getWaitTime()
 	return WaitTime;
 }
 
-int Passenger::getDirection()
+int Passenger::getDirection(int numFloor)
 {
+	srand(time(NULL));
 	while (Destination == CurrentFloor) {
 		//if they are already on the floor they want to go to, have them pick another floor
-		cout << "Your destination cannot be your current floor, try again\n";
-		cin >> Destination;
-		//or generate another random number
+		Destination = rand() % numFloor + 1; //random number from 1-number of floors 
+		cout << "Generating new destination... \n";
 	}
+
 	if (Destination > CurrentFloor)
 		return 1; //if they are going up
 	else if (Destination < CurrentFloor)
@@ -63,6 +65,11 @@ void Passenger::setArrivalTime(int AT)
 	ArrivalTime = AT;
 }
 
+void Passenger::setExitTime(int ET)
+{
+	ExitTime = ET;
+}
+
 void Passenger::setCurrentFloor(int CF)
 {
 	CurrentFloor = CF;
@@ -73,11 +80,14 @@ void Passenger::setDestination(int Destin)
 	Destination = Destin;
 }
 
+void Passenger::setDirection(int D)
+{
+	Direction = D;
+}
+
 void Passenger::setIDNum(int ID)
 {
 	IDNum = ID;
 }
-
-
 
 
