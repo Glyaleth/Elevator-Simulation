cb0cfd0b7a99be327e6903bb26edf11de5406338
saally61 <37127822+saally61@users.noreply.github.com>
2018-03-13 12:36:02 -0500

    Updated Main with updated tasks

--- Source.cpp
+++ Source.cpp
@@ -1,53 +1,44 @@
 #include <iostream>
 #include <string>
-#include<array>
+#include <array>
 #include <stack>
+#include <ctime>
+
 #include "Passenger.h"
 #include "Building.h"
 #include "Elevator.h"
 using namespace std;
 
 
-/*OVERALL THINGS THAT WE NEED TO DO:
+//***TODO*** BUILDING: once we get the #elevators we will make a vector of type elevators to keep track of n elevators 
+
+//My_Building.add_Passenger(the_passenger, the_passenger.getDirection());
+//***TODO*** BUILDING: Create function that takes in a passenger and pushes them into their queue (based on their direction)
 
 
-Building My_Building(e_num, p_num); //***TODO*** Create Constructor that takes in #elevator & #floors, assigns values and 'creates' the building
-//***TODO*** We need a way to keep track of all of our elevators, either in the Main or in the Building class...
+//My_Building.Decide(); 
+//***TODO*** BUILDING: function to iterate through each elevator and decide which floor to go to
+//***TODO*** BUILDING: decide function should call My_Elevator.move(next_floor); -->function to move elevator to next_floor, should return void
 
-Elevator My_Elevator(); //Contructor for the elevator?? Maybe this should be within Building??
-
-int new_arrivals = rand(); //***TODO*** create random function that generates a random # from (x,y)
-
-My_Building.add_Passenger(the_passenger, the_passenger.getDirection()); //***TODO*** Create function in Building that takes in a passenger and pushes them into their queue (based on their direction)
-
-My_Building.Decide(); //***TODO*** Building function iterate through each elevator and decide which floor to go to
-//***TODO*** Within Building, decide function should call My_Elevator.move(next_floor); -->function to move elevator to next_floor, should return void
-
-//Once arriving all elevators have moved to their new floor, pop out passengers
-//THIS NEEDS TO BE DONE FOR EACH ELEVATOR
-while (!My_Elevator.StillExiting()) { //***TODO*** Create a bool .StillExiting() that checks if there are still passengers that need to get "popped" out of the elevator
-
-Passenger exit_passenger = My_Elevator.Exit(); //***TODO*** Create function to decide who needs to get off on the current floor it is on
-//***TODO** .Exit() should somehow return the passenger that just exited the elevator
-
-//After popping out passengers, start pushing new passengers into elevator
-//THIS NEEDS TO BE DONE FOR EACH ELEVATOR
-
-while (!My_Elevator.StillLoading()) { //***TODO*** create bool StillLoading function that checks if there is still anyone on the current floor that needs to be loaded in
-
-Passenger loading_passenger; //**TODO*** how do we know who need to get on the elevator?
-
-My_Elevator.load(loading_passenger, loading_passenger.getDestination()); //***TODO*** Create Elevator function to load a passenger into the elevator
-//**Function takes in the passenger getting loaded, and their destination floor, and returns void??
+//while (!My_Elevator.StillExiting()) { //While there are still passengers that need to get off of the elevator 
+//***TODO*** ELEVATOR: Create a bool .StillExiting() that checks if there are still passengers that need to get "popped" out of the elevator 
 
 
+//Passenger exit_passenger = My_Elevator.Exit();
+//***TODO*** ELEVATOR: Create function to decide who needs to get off on the current floor it is on									  
+//***TODO*** ELEVATOR: .Exit() should return the passenger that just exited the elevator
 
-*/
 
+//while (!My_Elevator.StillLoading()) {//While there are still people that need to get into the elevator
+//***TODO*** ELEVATOR: create bool StillLoading function that checks if there is still anyone on the current floor that needs to be loaded in
+
+//My_Elevator.load(loading_passenger, loading_passenger.getDestination());
+//***TODO*** ELEVATOR: Create Elevator function to load a passenger into the elevator that takes in the passenger getting loaded, and their destination, and returns void??
 
 
 
 void PrintResults(int pass_num, stack <Passenger> Pass_Stack);
+void getInput(int &e_num, int &f_num, int &p_num);
 
 void main() {
 	bool again = true;
@@ -58,48 +49,55 @@ void main() {
 		cin >> choice;
 
 		if (choice == 1) {
-			int e_num, f_num, p_num; //#Elevator, #Floors, #passengers
-			cout << "Begin Elevator Simulation" << endl;
-			cout << "Please enter the number of elevators you want to see simulated(1 to 10):" << endl;
-			cin >> e_num;
-			cout << "Please enter the number of floors in the building" << endl;
-			cin >> f_num;
-			cout << "Please enter the number of passenger" << endl;
-			cin >> p_num;
+			int e_num=-1; //#elevators
+			int f_num=-1; //#floors
+			int p_num; //#passengers
+			
+			getInput(e_num, f_num, p_num); 
 
-			Building My_Building(e_num, p_num); //***TODO*** Create Constructor that takes in #elevator & #floors, assigns values and 'creates' the building
-			//***TODO*** We need a way to keep track of all of our elevators, either in the Main or in the Building class...
-			Elevator My_Elevator();
+			Building My_Building(e_num, f_num); //pass #elevators and #floors
+			
+			//***TODO*** BUILDING CLASS: once we get the #elevators we will make a vector of type elevators to keep track of n elevators 
+			Elevator My_Elevator(); 
 
-			int count = 0, num_arrived = 0, IDNum_Count = 0, num_exited; //counter to calculate wait time, #of passengers already arrived, #passengers who have exited the elevator
+			int count = 0; //counter to calculate what tick we are at
+			int num_arrived = 0; //counts the number of passengers already arrived
+			int IDNum_Count = 0; //counter to keep track of what ID numbers are already used
+			int num_exited=0; //counter of #passengers who have exited the elevator
+
 			stack <Passenger> Exitors; //Stack stores all passengers that have exited the elvevator 
 
-
 			while (num_exited < p_num) { //while there are still passengers who haven't made it to their destination
-				int new_arrivals = rand(); //***TODO*** create random function that generates a random # from (x,y)
+				srand(time(NULL));
+				int new_arrivals = rand() % 3 + 0; //random # from 0-3 for passengers that just arrived
 
 				//Create Passengers
 				for (int i = 0; i < new_arrivals; i++) {
-					Passenger the_passenger(count, IDNum_Count); //Create a passenger that arrived at the current count & assign them an IDNumber
+					Passenger the_passenger(count, IDNum_Count, f_num); //Create a passenger pass the current count & assign IDNumber
 					IDNum_Count++; //Increment IDNum_Counter to give each passenger a unique ID (based on priority, who got here first)
-					My_Building.add_Passenger(the_passenger, the_passenger.getDirection()); //***TODO*** Create function in Building that takes in a passenger and pushes them into their queue (based on their direction)
+
+					My_Building.add_Passenger(the_passenger, the_passenger.getDirection()); 
+					//***TODO*** BUILDING: Create function that takes in a passenger and pushes them into their queue (based on their direction)
 				}
 
 
 
 				//Decide where to go
-				//After adding the new passengers & their data, have the building manager decide what all elevators should do next
-				My_Building.Decide(); //***TODO*** Building function iterate through each elevator and decide which floor to go to
+				//After adding the new passengers & data, have the building manager decide what all elevators should do next
+				My_Building.Decide(); 
+					//***TODO*** Building function iterate through each elevator and decide which floor to go to
 					//***TODO*** Within Building, decide function should call My_Elevator.move(next_floor); -->function to move elevator to next_floor, should return void
 
 			
-				//Once arriving all elevators have moved to their new floor, pop out passengers
+				//Once all elevators have moved to their new floor, pop out passengers
 
 				//THIS NEEDS TO BE DONE FOR EACH ELEVATOR
-				while (!My_Elevator.StillExiting()) { //***TODO*** Create a bool .StillExiting() that checks if there are still passengers that need to get "popped" out of the elevator 
-					//While there are still passengers that need to get off of the elevator 
-					Passenger exit_passenger = My_Elevator.Exit(); //***TODO*** Create function to decide who needs to get off on the current floor it is on
-															  //***TODO** .Exit() should somehow return the passenger that just exited the elevator
+				while (!My_Elevator.StillExiting()) { //While there are still passengers that need to get off of the elevator 
+					//***TODO*** Create a bool .StillExiting() that checks if there are still passengers that need to get "popped" out of the elevator 
+					
+					Passenger exit_passenger = My_Elevator.Exit(); 
+						//***TODO*** Create function to decide who needs to get off on the current floor it is on									  
+						//***TODO** .Exit() should return the passenger that just exited the elevator
 
 					exit_passenger.setExitTime(count); //Set the time that the passenger left the elevator to the current count
 					
@@ -109,14 +107,20 @@ void main() {
 					num_exited++; //Increment the #passengers exited by 1
 				}
 
+
 				//After popping out passengers, start pushing new passengers into elevator
 				//THIS NEEDS TO BE DONE FOR EACH ELEVATOR 
-				//While there are still people that need to get into the elevator
-				while (!My_Elevator.StillLoading()) { //***TODO*** create bool StillLoading function that checks if there is still anyone on the current floor that needs to be loaded in
-					Passenger loading_passenger; //**TODO*** how do we know who need to get on the elevator? 
-					cout << "Passenger #" << loading_passenger.getIDNum() << " in on elevator #..." << " and is headed to floor #...\n";
-					My_Elevator.load(loading_passenger, loading_passenger.getDestination()); //***TODO*** Create Elevator function to load a passenger into the elevator
-																											//**Function takes in the passenger getting loaded, and their destination floor, and returns void??
+				
+				while (!My_Elevator.StillLoading()) {//While there are still people that need to get into the elevator
+					//***TODO*** create bool StillLoading function that checks if there is still anyone on the current floor that needs to be loaded in
+					
+					Passenger loading_passenger; 
+					//**TODO*** how do we know who need to get on the elevator? 
+
+					cout << "Passenger #" << loading_passenger.getIDNum() << " is on elevator #..." << " and is headed to floor #..." << loading_passenger.getDestination() << endl;
+					
+					My_Elevator.load(loading_passenger, loading_passenger.getDestination()); 
+					//***TODO*** Create Elevator function to load a passenger into the elevator that takes in the passenger getting loaded, and their destination, and returns void??
 
 				}
 				
@@ -174,4 +178,33 @@ void PrintResults(int pass_num, stack <Passenger> Pass_Stack)
 
 
 }
+
+void getInput(int &e_num, int &f_num, int &p_num) //#Elevator, #Floors, #passengers
+{
+	cout << "Begin Elevator Simulation" << endl;
+
+	cout << "Please enter the number of elevators you want to see simulated(1 to 10):" << endl;
+	cin >> e_num;
+	while (e_num <= 0 || e_num > 10) { 
+		cout << "You must enter a number between 1 and 10\n";
+		cin >> e_num;
+	}
+
+	cout << "Please enter the number of floors in the building" << endl;
+	cin >> f_num;
+
+	while (f_num <= 1 ) { //must have more than 1 floor
+		cout << "You must have at least 2 floors\n";
+		cin >> f_num;
+	}
+
+
+	cout << "Please enter the number of passenger" << endl;
+	cin >> p_num;
+	while (p_num <= 0) {
+		cout << "You must have at least 1 passenger\n";
+		cin >> p_num;
+	}
+}
+
 
