e7be22ffc35408bcb5c6db72781fb630b1cc2c1e
saally61 <37127822+saally61@users.noreply.github.com>
2018-03-13 13:18:51 -0500

    Update Building.cpp

--- Building.cpp
+++ Building.cpp
@@ -13,53 +13,73 @@
 #include <queue>
 using namespace std;
 
-/*void get_entry_pass(){
-    cout<<"initially,how many passenger entered the elevator"<<endl;
-    cin<<entry_passengers;
-}*/
-void passenger_entry(int i){
-    entry_passengers+=i;
-    
-}
-void passenger_exit(int n){
-    exit_passengers+=n;
-}
-int current_pass_in_ele(){
-    return entry_passengers-exit_passengers;
+
+
+Building::Building(int e_num, int f_num)
+{
+	elevators = e_num;
+	floors = f_num;
+
+	vector <Elevator> Elevator_vec;
+	vector <queue<Passenger>> Floor_vec;
+
+	for (int i = 0; i < elevators; i++) { //create i elevators and assign unique IDnumbers
+		Elevator my_elev(i);
+		Elevator_vec.push_back(my_elev);
+	}
+
+	for (int i = 0; i < floors; i++) { //create i floors, put a queue of passengers in each floor
+		queue <Passenger> PassengerQ();
+		Floor_vec.push_back(PassengerQ);
+	}
 }
 
-Building::Building()
+int Building::get_passengers()
 {
-    elevators = 0;
-    floors = 0;
+	return passengers;
 }
 
 int Building::get_elevators()
 {
-    return elevators;
+	return elevators;
 }
 
 int Building::get_floors()
 {
-    return floors;
+	return floors;
+}
+
+
+void Building::set_passengers(int p)
+{
+	passengers = p;
 }
 
 void Building::set_elevators(int e)
 {
-    elevators = e;
+	elevators = e;
 }
 
 void Building::set_floors(int f)
 {
-    floors = f;
-}
-void Elevator_Floor(e_num,f_num){
-    vector<Elevator>Elevator(e_num);
-    vector<queue<Passenger>>Floor;//queue stores the passenger waiting at that floor
-    Floor.push_back(queue<Passenger>()); // add a queue
-    Floor[0].push(Passenger());
+	floors = f;
 }
 
 
+
+int Building::current_pass_in_ele()
+{
+	return entry_passengers - exit_passengers;
+}
+
+void Building::passenger_entry(int i)
+{
+	entry_passengers += i;
+}
+
+void Building::passenger_exit(int n)
+{
+	exit_passengers += n;
+}
 
 
