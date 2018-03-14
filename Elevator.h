62519f9c4eddb1580033b8ad2f6782ef9392fab7
saally61 <37127822+saally61@users.noreply.github.com>
2018-03-14 00:48:42 -0500

    Update Elevator.h

--- Elevator.h
+++ Elevator.h
@@ -1,71 +1,48 @@
+#pragma once
+
+
 #include <queue>
-#include <vector>
 #include "Building.h"
 #include "Passenger.h"
+#include <vector>
 
+using namespace std;
 
+class Elevator{
 
-#pragma once
-class Elevator
-{
-public:
-
-	//-------------------Variables------------------
-	enum direction { down, up, idle };	// Direction.Up represents 0, Direction.down represents 1. direction.idle represents 2.
-	direction dir;
-	std::vector<Passenger> elev_passengers;		// Queue of passengers entering the elevator.
+public: 
+	vector <Passenger> elev_passengers; // vector of passengers entering the elevator.
 	int current_floor;				// Current floor of elevator in relation to 
 	bool emergency_button = false;
 	int elevator_number;			// Consider use as elevator ID.
+	bool idel;
 
 
+	//--------------Constructor--------------------
+	
+	Elevator(int ID);
 
-	//--------------Constructors--------------------
-	Elevator(int e_num);
-	Elevator(int ID, int _cur_floor) {  //Constructor accepts 2 values, elevator ID, and starting floor to assign to our current floor for initialization
-		
-		elevator_number = ID;
-		current_floor = _cur_floor;
-		dir = direction.idle;
-		elev_passengers.resize(10);
+	
+	//-------------Getters / Setters------------------------
 
-	}
-
+	int getCurrentFloor();
+	bool getEmergency_Button();
+	int getElevatorNumber();
+	vector <Passenger> getElevPass();
+	bool getIdel();
+	void setCurrentFloor(int f);
+	void setEmergency_Button(bool e);
+	void setElevatorNumber(int n);
+	void setIdel(bool i);
 
 	//-------------Functions------------------------
-	void setCurrentFloor(int floor_num) { // Setter for our current floor
-		current_floor = floor_num;
-	}
-	int getCurrentFloor() {
-		return current_floor;
-	}
 
-	bool still_exiting() {
-		for (int i = 0; i < elev_passengers.size(); i++) {
-			if (elev_passengers[i].getDestion() == current_floor)
-				return true;
-		}
-		return false;
-	}
+	bool still_exiting();
+	Passenger exit();
+	void load(Passenger passe);
 
 
-	Passenger exit() {
-		dir = direction.idle();
-		for (int i = 0; i < x.size()); i++){
-			if (x[i].getDestination() == current_floor) {
-				Passenger help = x[i];
-				x.pop(i);
-				return help;
-			}
-		}
-	}
-	void load(Passenger passe) {
-		elev_passengers.push_back(passe);
-		dir = direction.idle;
-	}
 
-	
-			
-	
+
 
 };
