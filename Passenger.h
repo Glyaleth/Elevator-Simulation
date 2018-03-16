2da614e403d9a8062f5a3cff6943861a53ed9490
saally61 <37127822+saally61@users.noreply.github.com>
2018-03-16 14:44:29 -0500

    Update Passenger.h

--- Passenger.h
+++ Passenger.h
@@ -5,14 +5,14 @@ private:
 	int ArrivalTime, CurrentFloor, Destination, ExitTime, WaitTime, Direction, IDNum;
 
 public:
-	Passenger(int arrive =-1, int ID = -1, int numFloor=-1);
+	Passenger(int arrive = -1, int ID = -1, int numFloor = -1);
 	int getArrivalTime(); //The time the passenger arrived
 	int getCurrentFloor(); //The floor the passenger is on
 	int getDestination(); //The floor the passenger wants to go
 	int getExitTime(int ET); //sets and returns time the passenger gets off the elevator
 	int getWaitTime(); //calculates, sets, and returns wait time based on currently assigned Arrive/Exit Time
-	int getDirection(int numFloor); //sets and returns Direction based on current/destination floor. 1=up, 0=down, -1=error-->current == destination floor 
 	int getIDNum();
+	int getDirection();
 
 	void setArrivalTime(int AT);
 	void setExitTime(int ET);
