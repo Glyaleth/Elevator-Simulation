e9b2bf3a068dc8aadf09921af5df28cb3aa14dc1
saally61 <37127822+saally61@users.noreply.github.com>
2018-03-18 22:45:10 -0500

    Update Building.cpp

--- Building.cpp
+++ Building.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
 #include "Building.h"
+#include <iostream>
 #include "Elevator.h"
 #include "Passenger.h"
 #include <vector>
@@ -11,14 +11,14 @@ Building::Building(int e_num, int f_num)
 	elevators = e_num;
 	floors = f_num;
 
-	for (int i = 1; i <= elevators; i++) { //create i elevators and assign unique IDnumbers
-		Elevator my_elev(i);
-		Elevator_vec.push_back(my_elev);
+	for (int i = 0; i < elevators; i++) { //create i elevators 
+		Elevator my_elev(i); //assign ID Number
+		Elevator_vec.push_back(my_elev); //Push into Elevator vector
 	}
 
-	for (int i = 1; i <= floors; i++) { //create i floors, put a queue of passengers in each floor
+	for (int i = 0; i < floors; i++) { //create i floors
 		queue <Passenger> PassengerQ;
-		Floor_vec.push_back(PassengerQ);
+		Floor_vec.push_back(PassengerQ); //put a queue of passengers in each index of the floor vector
 	}
 }
 
@@ -58,6 +58,11 @@ void Building::set_floors(int f)
 	floors = f;
 }
 
+vector<queue<Passenger>> Building::get_FloorVec()
+{
+	return Floor_vec;
+}
+
 int Building::current_pass_in_ele()
 {
 	return entry_passengers - exit_passengers;
@@ -88,48 +93,68 @@ void Building::add_Passenger(Passenger the_passenger)
 
 void Building::Decide()
 {
-	for (int i = 1; i <= Elevator_vec.size(); i++) {
+
+
+
+
+
+	for (int i = 0; i < Elevator_vec.size(); i++) { //For each elevator in the building
 		int d_floor = -1;
-		if (Floor_vec[i].empty() == false)  //if current floor has passengers
-			d_floor = i;
+		int c_floor = Elevator_vec[i].getCurrentFloor(); //get current floor of elevator
+		Elevator current = Elevator_vec[i]; //Save current elevator in temp variable
+
+		if (Floor_vec[c_floor].empty() == false)  //if current floor has passengers, stay on this floor
+			d_floor = c_floor;
 
 		else { //else move to another floor
-			//IF ELEVATOR NOT IN USE/IDEL
-			if (Elevator_vec[i - 1].getIdel() == false) { 
-				d_floor = Elevator_vec[i - 1].elev_passengers.front().getDestination();
+
+
+			//IF ELEVATOR NOT CURRENTLY IN USE/IDEL
+			if (current.getIdle() == false) {
+				d_floor = current.elev_passengers.front().getDestination();
 
 				//GOING UP: 
-				if (d_floor > Elevator_vec[i - 1].getCurrentFloor()) {
-					for (int p = 0; p < Elevator_vec[i - 1].elev_passengers.size(); p++) { //for every passenger in the elevator
-																						   //if someone else in the elevator is going somehwere alone the way (between front's destination and current floor) --> go there first
-						if ((Elevator_vec[i - 1].elev_passengers[p].getDestination() > Elevator_vec[i - 1].getCurrentFloor()) && (Elevator_vec[i - 1].elev_passengers[p].getDestination() < d_floor))
-							d_floor = Elevator_vec[i - 1].elev_passengers[p].getDestination();
+				if (d_floor > c_floor) {
+					for (int p = 0; p < current.elev_passengers.size(); p++) { //for every passenger in the elevator
+						//if someone's destination is between current and destination floor --> go there first
+						if ((current.elev_passengers[p].getDestination() > c_floor) && (current.elev_passengers[p].getDestination() < d_floor))
+							d_floor = current.elev_passengers[p].getDestination();
 					}
-					int cf = Elevator_vec[i - 1].getCurrentFloor(); //get current floor of elevator
-					for (cf + 1; cf < d_floor; cf++) { //for each floor on the way to d_floor
-						if (Floor_vec[i - 1].empty())
+
+					int cf = current.getCurrentFloor()+1; //temp variable for current floor
+					for (cf; cf < d_floor; cf++) { //for each floor on the way to d_floor
+						if (Floor_vec[cf].empty()) //if floor is empty, do nothing
 							continue;
-						else if (Floor_vec[i - 1].front().getDirection()) //if the first person at that floor is also going up
-							d_floor = Floor_vec[i - 1].front().getDestination();
+						else if (Floor_vec[cf].front().getDirection() == 1) { //if the first person at that floor is also going up
+							d_floor = Floor_vec[cf].front().getCurrentFloor();
+							break;
+						}
+						
 					}
 				}
+
 				//GOING DOWN:
-				else if (d_floor < Elevator_vec[i - 1].getCurrentFloor()) {
-					for (int p = 0; p < Elevator_vec[i - 1].elev_passengers.size(); p++) {
-						if ((Elevator_vec[i - 1].elev_passengers[p].getDestination() < Elevator_vec[i - 1].getCurrentFloor()) && (Elevator_vec[i - 1].elev_passengers[p].getDestination() > d_floor))
-							d_floor = Elevator_vec[i - 1].elev_passengers[p].getDestination();
+				else if (d_floor < c_floor) {
+					for (int p = 0; p < current.elev_passengers.size(); p++) { //for each passenger on the way to d_floor
+						//if someone's destination is between current and destination floor --> go there first
+						if ((current.elev_passengers[p].getDestination() < c_floor) && (current.elev_passengers[p].getDestination() > d_floor))
+							d_floor = current.elev_passengers[p].getDestination();
 					}
-					int cf = Elevator_vec[i - 1].getCurrentFloor(); //get current floor of elevator
-					for (cf - 1; cf > d_floor; cf--) { //for each floor on the way to d_floor
-						if (Floor_vec[i - 1].empty())
+					int cf = current.getCurrentFloor()-1; //temp variable for current floor
+					for (cf; cf > d_floor; cf--) { //for each floor on the way to d_floor
+						if (Floor_vec[cf].empty()) //if floor is empty, do nothing
 							continue;
-						else if (Floor_vec[i - 1].front().getDirection() == 0) //if the first person at that floor is also going down
-							d_floor = Floor_vec[i - 1].front().getDestination();
+						else if (Floor_vec[cf].front().getDirection() == 0) { //if the first person at that floor is also going down
+							d_floor = Floor_vec[cf].front().getCurrentFloor();
+							break;
+						}
 					}
 				}
 			}
+
+
 			//ELEVATOR NOT IN USE: 
-			else if (Elevator_vec[i - 1].getIdel() == true) {
+			else if (current.getIdle() == true) {
 				for (int k = 0; k < Floor_vec.size(); k++) {
 					if (!Floor_vec[k].empty()) { //if there is someone on this floor 
 						d_floor = k;//go to that floor
@@ -139,39 +164,40 @@ void Building::Decide()
 			}
 		}
 
-		if (d_floor == -1) { //if d floor didn't change
-			cout << "Elevator " << i << " went nowhere\n";
-		}
-		else { //move elevator and load passengers
-			cout << "Elevator " << i << " went from floor [" << Elevator_vec[i - 1].getCurrentFloor();
-			Elevator_vec[i - 1].setCurrentFloor(d_floor);
-			cout << "] to [" << Elevator_vec[i - 1].getCurrentFloor() << "]\n";
-			this->loading_passengers(i - 1, d_floor);
-		}
+		Elevator_vec[i].setCurrentFloor(d_floor);
+		this->loading_passengers(i, d_floor);
+
+
+
 	}
 }
 
 void Building::loading_passengers(int elev, int floor) //looks at all the elevators in the building, it's current floor and if it needs to load any passengers
 {
+
 	Passenger p;
-	while (!Floor_vec[floor].empty()) {  //while the current floor's queue is not empty
+	while (Floor_vec[floor].size()>0) {  //while the current floor's queue is not empty
 		p = Floor_vec[floor].front(); //save the passenger at the front of the queue
+		Floor_vec[floor].front().setOnElevator(true);
 		Elevator_vec[elev].load(p); //load into elevator
 		Floor_vec[floor].pop(); //remove from the queue
-		cout  << "LOADING: Passenger " << p.getIDNum() << " on floor [" << floor << "]\n";
+		cout << "LOADING: Passenger " << p.getIDNum() << " on floor [" << floor << "]\n";
 	}
+
 }
 
-int Building::unloading_passengers(int c, stack<Passenger>& Exitors)
+int Building::unloading_passengers(int count, stack<Passenger>& Exitors)
 {
 	int num_exited = 0;
 	for (int i = 0; i < Elevator_vec.size(); i++) {
 		while (Elevator_vec[i].still_exiting()) { //While there are still passengers that need to get off 
 			Passenger exit_passenger = Elevator_vec[i].exit();
-			exit_passenger.setExitTime(c); //Set the time that the passenger left the elevator to the current count
+			exit_passenger.setExitTime(count); //Set the time that the passenger left the elevator to the current count
 			Exitors.push(exit_passenger); //store the passenger into the stack of exited passengers
 			num_exited++; //Increment the #passengers exited by 1
 		}
 	}
 	return num_exited;
+
+
 }
